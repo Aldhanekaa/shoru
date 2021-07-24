ccbb0a1add1b8b013dd530ec1d3f4d2e221e1a9e
aldhanekaa <aldhanekadev@gmail.com>
2021-07-24 09:10:49 +0700

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { loadNuxt, build } = require('nuxt')
 
 const app = require('express')()
-// const isDev = process.env.NODE_ENV !== 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 
 async function start() {
